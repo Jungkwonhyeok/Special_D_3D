267604ad3720034080ed276548d9acc662ad2264
Jungkwonhyeok <786poqqep@gmail.com>
2025-12-29 21:19:19 +0900

    디펜스 1일차

---
+++ Assets/P1NGMU/Script/MapManager.cs
@@ -0,0 +1,148 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace P1NGMU
+{
+    [Serializable]
+    public class MapData
+    {
+        public BlockName blockName;
+        public int z;
+        public int x;
+
+        public MapData(int x, int z)
+        {
+            this.x = x;
+            this.z = z;
+        }
+
+        public Vector2 ToVector()
+        {
+            return new Vector2(x, z);
+        }
+        public static MapData operator +(MapData a, MapData b)
+            => new MapData(a.x + b.x, a.z + b.z);
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                return x == ((MapData)obj).x && z == ((MapData)obj).z;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+    public enum BlockName
+    {
+        Wall = 0, //0.0.0
+        NotWalkable = 1,  //255.255.255
+        Walkable = 2,  //153.217.234
+        Response = 3,  //237.28.36
+        BuildingLand = 4,  //181.230,29
+        DefenseBuilding = 5,  //255.242.0
+
+        Build = 6,    //지어진 상태
+        MOnster = 7,
+    }
+
+    public class MapManager : MonoBehaviour
+    {
+        List<MapData> mapDatas = new List<MapData>();
+        public Texture2D MapInfo;
+        public Color[] ColorBlock;
+        public int mapWidth;
+        public int mapHeight;
+        public GameObject[] Block;
+        public int blockScale = 1;
+        public Transform Map;
+        public List<MapData> mapData = new List<MapData>();
+        public List<MapData> directions = new List<MapData>()
+        {
+            new MapData(1, 0),
+            new MapData(0, 1),
+            new MapData(-1, 0),
+            new MapData(0, -1),
+        };
+
+        public void GenerateMap()
+        {
+            mapWidth = MapInfo.width;
+            mapHeight = MapInfo.height;
+            Debug.Log("mapWidth : " + mapWidth + ", mapHeight : " + mapHeight);
+            Color[] pixels = MapInfo.GetPixels();
+
+            for (int i = 0; i < mapHeight; i++)
+            {
+                for (int j = 0; j < mapWidth; j++)
+                {
+                    Color pixelColor = pixels[i * mapWidth + j];
+                    MapData data = new MapData(j, i);
+                    if(pixelColor == ColorBlock[(int)BlockName.Wall])
+                    {
+                        Instantiate(Block[(int)BlockName.Wall],
+                            new Vector3(blockScale * j, 0,
+                            blockScale *i), Quaternion.identity, Map);
+                        data.blockName = BlockName.Wall;
+                    }
+                    else if(pixelColor == ColorBlock[(int)BlockName.Walkable])
+                    {
+                        Instantiate(Block[(int)BlockName.Walkable],
+                            new Vector3(blockScale * j, 0, blockScale * i),
+                            Quaternion.identity, Map);
+                        data.blockName = BlockName.Walkable;
+                    }
+                    else if (pixelColor == ColorBlock[(int)BlockName.NotWalkable])
+                    {
+                        Instantiate(Block[(int)BlockName.NotWalkable],
+                            new Vector3(blockScale * j, 0, blockScale * i),
+                            Quaternion.identity, Map);
+                        data.blockName = BlockName.NotWalkable;
+                    }
+                    else if (pixelColor == ColorBlock[(int)BlockName.Response])
+                    {
+                        Instantiate(Block[(int)BlockName.Response],
+                            new Vector3(blockScale * j, 0, blockScale * i),
+                            Quaternion.identity, Map);
+                        data.blockName = BlockName.Response;
+                    }
+                    else if (pixelColor == ColorBlock[(int)BlockName.DefenseBuilding])
+                    {
+                        Instantiate(Block[(int)BlockName.DefenseBuilding],
+                            new Vector3(blockScale * j, 0, blockScale * i),
+                            Quaternion.identity, Map);
+                        data.blockName = BlockName.DefenseBuilding;
+                    }
+                    else if (pixelColor == ColorBlock[(int)BlockName.BuildingLand])
+                    {
+                        Instantiate(Block[(int)BlockName.BuildingLand],
+                            new Vector3(blockScale * j, 0, blockScale * i),
+                            Quaternion.identity, Map);
+                        data.blockName = BlockName.BuildingLand;
+                    }
+                    mapData.Add(data);
+                }
+            }
+        }
+
+        void Start()
+        {
+            Map = GameObject.Find("MAP").transform;
+            GenerateMap();
+        }
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
